8f8d05595d83ff0532a92df31fa1b26f1c52b992
Daria <dariaurbanik@gmail.com>
2019-04-01 22:20:49 +0200

    Toggle switch for users

--- src/components/UserBoard.js
+++ src/components/UserBoard.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
 
 export default class UserBoard extends Component {
+    state = { active: true }
+
+    toggle = () => { 
+        this.setState({ //setState is used when we want action to happen
+          active: !this.state.active
+        })
+      }
+    
   render() {
     return  <div>
          <h3>User Board</h3>
-      <div><p>User: <b>Alice</b></p></div>
-      <div><p>User: <b>Bob</b></p></div>
-      <dic><p>User: <b>Charles</b></p></dic>
+      <div><p>User: <b>Alice</b></p>
+      <p>Alice is { this.state.active ? 'present' : 'not present' }</p></div>
+      <button onClick={this.toggle}>Toggle</button>
+      <div><p>User: <b>Bob</b></p>
+      <p>Bob is { this.state.active ? 'present' : 'not present' }</p></div>
+      <button onClick={this.toggle}>Toggle</button>
+      <div><p>User: <b>Charles</b></p>
+      <p>Charles is { this.state.active ? 'present' : 'not present' }</p></div>
+      <button onClick={this.toggle}>Toggle</button>
     </div>
   }
 }
