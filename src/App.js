ce4062f16a678f57a8288e603bb7d603f61a4b12
Daria <dariaurbanik@gmail.com>
2019-04-01 15:59:00 +0200

    Light switch added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title'
+import LightSwitch from './components/LightSwitch'
 
 class App extends Component {
   render() {
@@ -8,19 +10,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Codaisseur</h2>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
+
+        <main>
+          <Title content="Some Simple Title" />
+          <LightSwitch />
+        </main>
       </div>
     );
   }
